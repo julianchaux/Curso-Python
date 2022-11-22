30ed3c3b3f9bcb11f1deac82650c03106a4f48cd
Julián René Cháux Cedeño <jchaux@misena.edu.co>
2022-11-21 23:53:14 -0500

    Upload Caesar Cipher

---
+++ 5. Beginner - Caesar Cipher/main.py
@@ -0,0 +1,38 @@
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+def encrypt(text, shift):
+  new_key = ""
+  shift1 = shift
+  for i in text:
+    index1 = alphabet.index(i)
+    #26
+    if index1 + shift > len(alphabet)-1:
+      shift1 = index1 + shift - len(alphabet)
+      new_key += alphabet[shift1]
+    else:
+      shift1 = shift
+      new_key += alphabet[index1 + shift1]    
+  print(f"The encoded text is {new_key}")
+
+def decrypt(text, shift):
+  new_key = ""
+  shift1 = shift
+  for i in text:
+    index1 = alphabet.index(i)
+    #26
+    if index1 - shift < 0:
+      shift1 = index1 - shift
+      new_key += alphabet[shift1]
+    else:
+      shift1 = shift
+      new_key += alphabet[index1 - shift1]    
+  print(f"The encoded text is {new_key}")
+
+direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+text = input("Type your message:\n").lower()
+shift = int(input("Type the shift number:\n"))
+
+if direction == "encode":
+  encrypt(text, shift)
+elif direction == "decode":
+  decrypt(text, shift)

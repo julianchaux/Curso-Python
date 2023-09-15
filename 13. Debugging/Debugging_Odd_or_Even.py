17e81acaaf2019c4badfc8d2c7118d530782ceed
Julián René Cháux Cedeño <jchaux@misena.edu.co>
2023-09-15 00:11:01 -0500

    Create Debugging_Odd_or_Even.py

---
+++ 13. Debugging/Debugging_Odd_or_Even.py
@@ -0,0 +1,26 @@
+# Debugging Odd or Even
+
+# Instructions
+# Read this the code in main.py
+# Spot the problems 🐞.
+# Modify the code to fix the program.
+# Fix the code so that it works and passes the tests when you submit.
+
+# Hint
+# Review the previous lesson and go through the 10 steps to tackle these debugging problems.
+
+# Code Before Debugging
+# number = int(input("Which number do you want to check?"))
+
+# if number % 2 = 0:
+#   print("This is an even number.")
+# else:
+#   print("This is an odd number.")
+
+#Code After Debugging
+number = int(input("Which number do you want to check?"))
+
+if number % 2 == 0:
+  print("This is an even number.")
+else:
+  print("This is an odd number.")

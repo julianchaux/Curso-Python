081e1319a607208aca1cf79bd03467bb1b03993d
Julián René Cháux Cedeño <jchaux@misena.edu.co>
2024-01-25 22:37:21 -0500

    Routes

--- Web Development Projects/1. Flask/1. Hello Flask/hello.py
+++ Web Development Projects/1. Flask/1. Hello Flask/hello.py
@@ -5,7 +5,11 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "<p>Hello, World!</p>"
+    return "<p>Hola, estoy en la ruta principal!</p>"
+
+@app.route("/second_page")
+def page_secondary():
+    return "<h1>Hola estoy en la ruta secundaria</h1>"
 
 if __name__ == "__main__":
     app.run()

98a89164722bdbcc1e71a5774a8caea91ae2fa76
Julián René Cháux Cedeño <jchaux@misena.edu.co>
2024-01-30 23:43:13 -0500

    Multiline JINJA

---
+++ Web Development Projects/1. Flask/8. Multiline JINJA/server.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template
+import requests
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def home():
+    return render_template("index.html")
+
+@app.route('/blog')
+def blog():
+    #Fake blog: https://www.npoint.io/docs/c790b4d5cab58020d391
+    response = requests.get(url="https://api.npoint.io/c790b4d5cab58020d391")
+    response.raise_for_status()
+    all_posts = response.json()
+    
+    return render_template("blog.html", posts=all_posts)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)

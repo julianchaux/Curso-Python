8978a4616a64631c0ca2dd8d35303565a9322dc6
Julián René Cháux Cedeño <jchaux@misena.edu.co>
2024-01-30 22:23:37 -0500

    Jinja Rendering Templates

--- Web Development Projects/1. Flask/6. Jinja Template/main.py
+++ Web Development Projects/1. Flask/6. Jinja Template/main.py
@@ -1,11 +1,18 @@
 from flask import Flask, render_template
+import random
+from datetime import datetime
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def home():
-    return "Hello World!"
+    random_number = random.randint(0, 10)
+    current_year = datetime.now().year
+    print(current_year)
+    name = "Julian Chaux"
+    #Se envían argumentos con nombre **kwargs
+    return render_template("index.html", num=random_number, current_year=current_year, name=name)
 
 
 if __name__ == "__main__":
